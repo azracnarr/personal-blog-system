6b44c002d213111882c5544de909ebbccd13a4e9
azra çınar <132001216+azracnarr@users.noreply.github.com>
2026-09-01 01:54:55 +0300

    Add Windows project environment activator

---
+++ work/activate-project-env.cmd
@@ -0,0 +1,7 @@
+@echo off
+set "JAVA_HOME=C:\Program Files\Java\jdk-21"
+set "PATH=%JAVA_HOME%\bin;C:\Program Files\PostgreSQL\14\bin;C:\apache-maven-3.9.9\bin;C:\Program Files\nodejs;%PATH%"
+
+echo Project environment activated.
+echo JAVA_HOME=%JAVA_HOME%
+echo Use npm.cmd instead of npm if PowerShell blocks npm.ps1.
